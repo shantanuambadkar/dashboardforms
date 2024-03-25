4b75818a68f2224df8bd6368b27b3fb8b9cdd1ef
Shantanu Ambadkar <89132314+shantanuambadkar@users.noreply.github.com>
2024-03-25 13:06:37 +0530

    Added InBank & SmartForms logo

--- src/components/ui/LoginPageBankName.js
+++ src/components/ui/LoginPageBankName.js
@@ -1,10 +1,24 @@
 import { Grid } from '@material-ui/core';
 import '../../css/LoginPage.css';
+import InBankLogo from '../icons/inbank_blue.png';
+import SmartFormsLogo from '../icons/smartforms_blue.png';
 
 function LoginPageBankName() {
   return (
     <Grid container className="bank-name-text">
-      <h1>The ABC Co-Operative Bank</h1>
+      <Grid item xs={3}>
+        <img className="inbankLogo" src={InBankLogo} alt="InBank Blue Logo" />
+      </Grid>
+      <Grid item xs={6}>
+        <h1>The ABC Co-Operative Bank</h1>
+      </Grid>
+      <Grid item xs={3}>
+        <img
+          className="inbankLogo"
+          src={SmartFormsLogo}
+          alt="InBank Blue Logo"
+        />
+      </Grid>
     </Grid>
   );
 }
