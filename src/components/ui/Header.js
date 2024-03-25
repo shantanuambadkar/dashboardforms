e4db26756e6c617d926d7f8d7a85cdf358ae7cf9
Shantanu Ambadkar <89132314+shantanuambadkar@users.noreply.github.com>
2024-03-25 11:08:55 +0530

    Added Logout icon in Header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../css/Common.css';
 import '../../css/Header.css';
+import LogoutIcon from '../icons/icons8-login-100.png';
 
 function Header() {
   const navigate = useNavigate();
   const goToDashboard = () => {
+    navigate('/dashboard');
+  };
+
+  const handleLogout = () => {
     navigate('/');
   };
+
   return (
     <div className="App-header">
       <span
@@ -16,6 +22,17 @@ function Header() {
       >
         The ABC Co-Operative Bank
       </span>
+      <div className="logout-div">
+        <span className="logout-font cursor-pointer" onClick={handleLogout}>
+          Logout
+        </span>
+        <img
+          className="logout-icon cursor-pointer"
+          src={LogoutIcon}
+          alt="Logout Door"
+          onClick={handleLogout}
+        />
+      </div>
     </div>
   );
 }
